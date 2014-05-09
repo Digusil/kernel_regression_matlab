5effdf4b5f0dc356db849db718422ae35933b6a3
Digusil <pircher.th@googlemail.com>
2014-05-09 21:17:24 +0200

    estimation update

--- estimateH.m
+++ estimateH.m
@@ -5,12 +5,12 @@ function [h_est, rho] = estimateH(x)
     x2 = sum(x.^2, 2);
     u = sqrt(abs(bsxfun(@plus, x2, bsxfun(@plus, x2', - 2 * (x * x')))));
     
-%     h_est = 0.35./(m.^0.2)*median(u(:));
 %    h_est = 0.178 * m^-0.2597 * n^0.8986 * median(u(:));
     h_est = 0.22 * m^-0.185 * n^0.45 * median(u(:));
     
     if nargout > 1
-        rho = 1/m* sum(u,2);
+        u(u==0) = NaN;
+        rho = 1/m * min(abs(u),[],2).^n;
     end
 
 end
