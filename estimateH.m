cc97e8bf9b090133d65a7da2f539c34963750baa
digusil <pircher.th@googlemail.com>
2014-05-11 10:25:49 +0200

    delete previous estimation formula

--- estimateH.m
+++ estimateH.m
@@ -5,7 +5,6 @@ function [h_est, rho] = estimateH(x)
     x2 = sum(x.^2, 2);
     u = sqrt(abs(bsxfun(@plus, x2, bsxfun(@plus, x2', - 2 * (x * x')))));
     
-%    h_est = 0.178 * m^-0.2597 * n^0.8986 * median(u(:));
     h_est = 0.22 * m^-0.185 * n^0.45 * median(u(:));
     
     if nargout > 1
